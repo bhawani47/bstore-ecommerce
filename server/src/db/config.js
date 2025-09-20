c5a7b5b4c97c39df7ff67e425b18629c8b861655
bhawani47 <120445101+bhawani47@users.noreply.github.com>
2025-09-20 20:11:23 +0530

    Setup backend

---
+++ server/src/db/config.js
@@ -0,0 +1,17 @@
+import { DB_NAME } from '../constants.js';
+import asyncHandler from '../utils/asyncHandler.js';
+import mongoose from 'mongoose';
+
+export const connectDB = async () => {
+  try {
+    const connResponse = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      'MongoDB Connected successfully with : ' + connResponse.connection.host
+    );
+  } catch (error) {
+    console.log('MONGODB connection FAILED ', error);
+    process.exit(1);
+  }
+}
